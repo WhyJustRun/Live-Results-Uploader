4591a31c0737ae994f2e97665aa5644ec91828a4
Russell Porter <contact@russellporter.com>
2014-03-07 21:14:48 -0800

    Add support for the way MeOS outputs files

--- live-results-uploader.py
+++ live-results-uploader.py
@@ -2,6 +2,7 @@ import requests
 from time import sleep
 import os.path
 from datetime import datetime
+import glob
 
 class LiveResultsUploader:
     def __init__(self):
@@ -10,6 +11,8 @@ class LiveResultsUploader:
        self.user_password = None
        self.event_id = None
        self.needs_user_info = True
+       self.last_meos_file_name = None
+       self.upload_interval = 5
 
     def results_url(self):
         return "https://whyjustrun.ca/iof/3.0/events/" + str(self.event_id) + "/live_result_list.xml"
@@ -20,15 +23,32 @@ class LiveResultsUploader:
             self.event_id = int(input("Enter the event ID (look for the number in the URL to the event page on WhyJustRun): "))
         self.user_email = input("Enter your WhyJustRun account email: ")
         self.user_password = input("Enter your WhyJustRun account password: ")
-        while (self.results_path is None or not os.path.isfile(self.results_path)):
-            self.results_path = input("Enter the path to the IOF XML 3.0 results list file: ")
+        while (self.results_path is None or (self.results_path.lower() != 'meos' and not os.path.isfile(self.results_path))):
+            self.results_path = input("Enter the path to the IOF XML 3.0 results list file (if using MeOS, just type \"meos\"): ")
 
         print("Ok, that's all we need. Now go to the event page to start showing the uploaded results publicly.")
 
     def upload_results(self):
-        results_file = open(self.results_path, 'r')
-        results = results_file.read()
-        results_file.close()
+        f = None
+        if (self.results_path.lower() == 'meos'):
+            files = glob.glob('[0-9]*[0-9]*[0-9]*[0-9]*.xml')
+            if (len(files) == 0):
+                print("Error loading results. Didn't find any Meos result files matching format 0123.xml.")
+                return
+            else:
+                sorter = lambda f: int(f.split('.')[0])
+                files = sorted(files, key=sorter)
+                name = files[-1]
+                if not self.last_meos_file_name is None and self.last_meos_file_name == name:
+                    return
+                self.last_meos_file_name = name
+                print("Uploading " + name)
+                f = open(name, 'r')
+        else:
+            f = open(self.results_path, 'r')
+        
+        results = f.read()
+        f.close()
         
         r = requests.post(self.results_url(), data=results, auth=(self.user_email, self.user_password))
         if (r.status_code != requests.codes.ok):
@@ -45,7 +65,7 @@ class LiveResultsUploader:
                 self.prompt_for_user_information()
 
             self.upload_results()
-            sleep(30)
+            sleep(self.upload_interval)
 
 
         
